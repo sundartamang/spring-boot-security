31a46100f0bcd83f2e509139d7f8d525a2524047
Sundar Tamang <sundartamang@Sundars-MacBook-Air.local>
2025-02-01 19:26:40 +0545

    feat: intial commit

---
+++ src/test/java/spring_boot_security/Spring/Boot/Security/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_security.Spring.Boot.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
